c57330f5ef59498cea8d9b203ebcc458e49fba43
kevin astrada <kevin.agustin.astrada@gmail.com>
2023-11-07 11:06:03 -0300

    Commit screenshot

---
+++ Homework_8/homework.js
@@ -0,0 +1,77 @@
+/*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
+
+function mayuscula(nombre) {
+   // Esta función recibe un nombre (string).
+   // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
+   // [Ejemplo]: "mario" ----> "Mario".
+   // Tu código:
+   return nombre[0].toUpperCase() + nombre.slice(1);
+
+}
+
+function invocarCallback(cb) {
+   // Invoca/ejecuta el callback `cb`.
+   // [NOTA]: no debes reotrnar nada.
+   // Tu código:
+  cb()
+   
+}
+
+function operacionMatematica(num1, num2, cb) {
+   // En este ejercicio recibirás dos números y un callback.
+   // El callback realiza una operación matemática, por lo que necesita de los dos números.
+   // Retorna el resultado del callback pasándole como parámetros los números.
+   // Tu código:
+ var resultado = cb(num1, num2);
+ return resultado;
+}
+
+function sumarArray(arrayOfNumbers, cb) {
+   // Recibes un arreglo de números y un callback.
+   // Suma todos los números del arreglo.
+   // Este resultado debes pasárselo como argumento al callback recibido.
+   // [NOTA]: no debes reotrnar nada.
+   // Tu código:
+  suma = 0;
+  for(var i = 0; i < arrayOfNumbers.length;i++){
+   suma += arrayOfNumbers[i]
+  }
+  cb(suma)
+}
+
+function forEach(array, cb) {
+   // Recibes un arreglo y un callback.
+   // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
+   // Debes pasarle el elemento como argumento al callback.
+   // Tu código:
+   array.forEach(elemento => cb(elemento))
+}
+
+function map(array, cb) {
+   // Debes iterar sobre el arreglo, y cada elemento pasárselo como arguemento al callback.
+   // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
+   // Retorna el nuevo arreglo.
+   // Tu código:
+   var nuevoArray = array.map(elemento =>cb(elemento))
+   return nuevoArray;
+}
+
+function filter(arrayOfStrings) {
+   // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
+   // Luego retorna un nuevo arreglo con estos elementos.
+   // Tu código:
+   
+   var filter = arrayOfStrings.filter((elemento)=> elemento.startsWith("a"));
+   return filter;
+}
+
+/*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
+module.exports = {
+   mayuscula,
+   invocarCallback,
+   operacionMatematica,
+   sumarArray,
+   forEach,
+   map,
+   filter,
+};
---
+++ Homework_9/Ejercicios Extra.js
@@ -0,0 +1,127 @@
+/*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
+
+function deObjetoAarray(objeto) {
+   // Recibes un objeto. Tendrás que crear un arreglo de arreglos.
+   // Cada elemento del arreglo padre será un nuevo arreglo que contendrá dos elementos.
+   // Estos elementos debe ser cada par clave:valor del objeto recibido.
+   // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
+   // Tu código:
+   var newArray = []
+   for(var i in objeto) {
+      newArray.push([i,objeto[i]]);
+   }
+   return newArray
+}
+
+function numberOfCharacters(string) {
+   // La función recibe un string. Debes recorrerlo y retornar un objeto donde cada propiedad es una de las
+   // letras del string, y su valor es la cantidad de veces que se repite en el string.
+   // Las letras deben estar en orden alfabético.
+   // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
+   // Tu código:
+
+   return string.split("").reduce((acc, e) => {
+     if (acc[e] === undefined) {
+       acc[e] = 1;
+     } else {
+       acc[e] += 1;
+     }
+     return acc;
+   }, {});
+}
+
+function capToFront(string) {
+   // Recibes un string con algunas letras en mayúscula y otras en minúscula.
+   // Debes enviar todas las letras en mayúscula al comienzo del string.
+   // Retornar el string.
+   // [EJEMPLO]: soyHENRY ---> HENRYsoy
+   // Tu código:
+      var letraMayuscula = "";
+      var letraMinuscula = "";
+  
+      for (var i = 0; i < string.length; i++) {
+          if (string[i] === string[i].toUpperCase()) {
+              letraMayuscula += string[i];
+          } else {
+              letraMinuscula += string[i];
+          }
+      }
+      return letraMayuscula + letraMinuscula;
+  }
+ 
+
+ 
+
+function asAmirror(frase) {
+   // Recibes una frase. Tu tarea es retornar un nuevo string en el que el orden de las palabras sea el mismo.
+   // La diferencia es que cada palabra estará escrita al inverso.
+   // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
+   // Tu código:
+   var palabra = frase.split(" ");
+   var palabraInvertida = palabra.map(palabra => {
+       return palabra.split("").reverse().join("");
+   });
+   var unirPalabraInvertida = palabraInvertida.join(" ")
+   return unirPalabraInvertida;
+}
+
+function capicua(numero) {
+   // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
+   // Caso contrario: "No es capicua".
+   // Tu código:
+   var prueba = numero.toString();
+   var resultado = prueba.split("").reverse().join("")
+   if(prueba === resultado){
+      return "Es capicua";
+   }else{
+      return "No es capicua";
+   }
+}
+
+function deleteAbc(string) {
+   // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
+   // Retorna el string sin estas letras.
+   // Tu código:
+   var modificarSring = string.replace(/[abc]/g, "")
+   return modificarSring;
+}
+
+function sortArray(arrayOfStrings) {
+   // Recibes un arreglo de strings.
+   // Debe retornar un nuevo arreglo, pero con las palabras ordenadas en orden creciente a partir
+   // de la longitud de cada string.
+   // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
+   // Tu código:
+   return arrayOfStrings.sort((a, b) => a.length - b.length);
+}
+
+function buscoInterseccion(array1, array2) {
+   // Recibes dos arreglos de números.
+   // Debes retornar un nuevo arreglo en el que se guarden los elementos en común entre ambos arreglos.
+   // [EJEMPLO]: [4,2,3] U [1,3,4] = [3,4].
+   // Si no tienen elementos en común, retornar un arreglo vacío.
+   // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
+   // Tu código:
+   var resultado = [];
+   for (var num of array1) {
+       if (array2.includes(num)) {
+           resultado.push(num);
+       }
+   }
+   return resultado;
+       }
+   
+ 
+
+
+/*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
+module.exports = {
+   deObjetoAarray,
+   numberOfCharacters,
+   capToFront,
+   asAmirror,
+   capicua,
+   deleteAbc,
+   sortArray,
+   buscoInterseccion,
+};
